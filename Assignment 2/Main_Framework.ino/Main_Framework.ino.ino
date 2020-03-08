8f4510b0eea81a823af81da103262dca2b6b9b45
Max <maxvangogh@home.nl>
2020-03-08 11:06:00 +0100

    Assignment 2 framework

---
+++ Assignment 2/Main_Framework.ino/Main_Framework.ino.ino
@@ -0,0 +1,30 @@
+//
+/// LIBRARIES ///
+#include <Wire.h>            // I2C library
+#include "SSD1306Wire.h"     // OLED display
+#include <Adafruit_Sensor.h> // Support library for BME280
+#include <Adafruit_BME280.h> // Library for BME280
+/// END OF LIBRARIES ///
+//
+
+//
+/// CONSTANTS ///
+
+/// END OF CONSTANTS ///
+//
+
+//
+/// VARIABLES ///
+
+/// END OF VARIABLES ///
+//
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
