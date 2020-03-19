23afff14e0a93555a294260f61043985ecd7b787
Max <maxvangogh@home.nl>
2020-03-19 10:21:24 +0100

    Added analog input support

---
+++ Assignment 2/Main_Framework.ino/Analog_Control.ino
@@ -0,0 +1,54 @@
+///
+/// Analog sensor control
+///
+/// readLDR() -> Reads the value from the ldr and returns it
+/// readSoilMoisture() -> Reads the value from the soilmoisture sensor and returns it
+///
+
+//
+/// VARIABLES ///
+// The state of the selector pin of the analog board
+int analogState = LOW;
+/// END OF VARIABLES ///
+//
+
+//
+/// EXTERNAL FUNCTIONS ///
+// Reads the value from the ldr and returns it
+int readLDR()
+{
+  switchAnalogToLDR();
+  return analogRead(ANALOG_INPUT);
+}
+
+// Reads the value from the soilmoisture sensor and returns it
+int readSoilMoisture()
+{
+  if(analogState == LOW)
+  {
+    switchAnalogToMoisture();
+  }
+  return analogRead(ANALOG_INPUT);
+}
+/// END OF EXTERNAL FUNCTIONS ///
+//
+
+//
+/// INTERNAL FUNCTIONS ///
+// Switch the input of the analog board to the ldr
+void switchAnalogToLDR()
+{
+  analogState = LOW;
+  digitalWrite(ANALOG_SELECT_PIN, analogState);
+}
+
+// Switch the input of the analog board to the moisture sensor
+void switchAnalogToMoisture()
+{
+  analogState = HIGH;
+  digitalWrite(ANALOG_SELECT_PIN, analogState);
+  // Give the soil moisture sensor some time to stabilize, also give the nodeMCU some time to do WiFi maintanance.
+  delay(100);
+}
+/// END OF INTERNAL FUNCTIONS ///
+//
